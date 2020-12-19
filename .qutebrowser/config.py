da202bac9e93f453090a9a2a49e0e55f5506e1c2
Daniel Hwang <danielleehwang@gmail.com>
2020-12-19 13:07:35 -0800

    qutebrowser/config.py

---
+++ .qutebrowser/config.py
@@ -0,0 +1,11 @@
+c.url.start_pages = "https://google.com/"
+c.url.searchengines = {
+    "DEFAULT": "https://google.com/search?q={}",
+    "code": "https://github.com/search?utf8=✓&q={}&type=Code",
+    "dd": "https://www.github.com/dangerdevices/{}",
+    "w": "https://en.wikipedia.org/w/index.php?title=Special:Search&search={}",
+    "y": "https://www.youtube.com/results?search_query={}",
+}
+
+c.editor.command = ["mvim", "-f", "{file}", "-c", "normal {line}G{column0}l"]
+config.bind("e", "open-editor", mode="insert")
