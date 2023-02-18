9859ef06267b52a5222390fc2d166c1f82604687
Daniel Hwang <danielleehwang@gmail.com>
2023-02-17 18:07:13 -0800

    :wrench:

--- .fzf.bash
+++ .fzf.bash
@@ -1,8 +1,9 @@
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
-source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
 
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
-# bind "$(bind -s | grep '^"\\C-r"' | grep -v '\\C-m' | sed 's/"$/\\C-m"/')"
+source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+
+bind "$(bind -s | grep '^"\\C-r"' | grep -v '\\C-m' | sed 's/"$/\\C-m"/')"
